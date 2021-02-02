30fc41ccb9ae98a8a1a8f2292d305e7a30b99678
Kuntal Majumder <kuntal@infracloud.io>
2021-02-02 12:21:41 +0530

    forgot sudo

--- tetrateci/create_istio_release.sh
+++ tetrateci/create_istio_release.sh
@@ -14,7 +14,7 @@ if [[ ${BUILD} == "fips" ]]; then
     url="https://go-boringcrypto.storage.googleapis.com/go1.15.7b5.linux-amd64.tar.gz"
     wget -O go.tgz "$url"
     echo "cb08962897e3802cda96f4ee915ed20fbde7d5d85e688759ef523d2e6ae44851 go.tgz" | sha256sum -c -
-    tar -C /usr/local -xzf go.tgz
+    sudo tar -C /usr/local -xzf go.tgz
     rm go.tgz
     export PATH="/usr/local/go/bin:$PATH"
     echo "FIPS compliant Go installed"
