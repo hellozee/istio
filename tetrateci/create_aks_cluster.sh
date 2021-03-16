dd90785732a5cda129b7a314e7ef8a3e703d216c
hellozee <github-actions@github.com>
2021-03-16 05:19:13 +0000

    Merging tetrate-workflow with tetrate-release-1.9

--- tetrateci/create_aks_cluster.sh
+++ tetrateci/create_aks_cluster.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+./tetrateci/version_check.py && exit
+
 set -o errexit
 set -o pipefail
+
 SHA8=$(git rev-parse --short $GITHUB_SHA)
-SUFFIX=$(sed 's/\.//g' <<< $VER)
+SUFFIX=$(sed 's/\.//g' <<< $K8S_VERSION)
 CLUSTER_NAME="test-istio-$SHA8-$SUFFIX"
-az aks create --resource-group $RESOURCE --name $CLUSTER_NAME --node-count 2 --generate-ssh-keys --kubernetes-version $VER -s standard_d8s_v3 --network-plugin azure
+
+echo "Fetching location of the resource"
+location=$(az group show -g $RESOURCE | jq '.location')
+
+echo "Fetching available kubernetes patch version for $K8S_VERSION"
+version=$(az aks get-versions -l $location | jq '.orchestrators[] | .orchestratorVersion' | grep $K8S_VERSION | tail -n 1)
+
+echo "Kubernetes version selected: $version"
+
+az aks create --resource-group $RESOURCE --name $CLUSTER_NAME --node-count 3 --generate-ssh-keys --kubernetes-version $version -s standard_ds3_v2 --network-plugin azure
 az aks get-credentials --resource-group $RESOURCE --name $CLUSTER_NAME
