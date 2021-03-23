ba79afbcdfcc7ddd95c274680214459153459efd
hellozee <github-actions@github.com>
2021-03-23 10:32:27 +0000

    Merging tetrate-workflow with tetrate-release-1.8

---
+++ tetrateci/setup_boring_go.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+if $(grep "1.7" <<< $TAG); then
+  export GOLANG_VERSION=1.14.12b4
+fi
+
+if $(grep -q "1.8" <<< $TAG || grep -q "1.9" <<< $TAG); then
+  export GOLANG_VERSION=1.15.8b5
+fi
+
+echo "Fetching FIPS compliant Go"
+url="https://go-boringcrypto.storage.googleapis.com/go$GOLANG_VERSION.linux-amd64.tar.gz"
+
+wget -O go.tgz "$url"
+
+sudo tar -C /usr/local -xzf go.tgz
+rm go.tgz
+
+export GOROOT=/usr/local/go
+export PATH="$GOROOT/bin:$PATH"
+
+echo "FIPS compliant Go installed"
+go version
