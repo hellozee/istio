3d990a21c6bf714439b0c683d4efc3da80a28d47
hellozee <github-actions@github.com>
2021-03-23 10:30:39 +0000

    Merging tetrate-workflow with tetratefips-release-1.9

--- tetrateci/test_1.8.sh
+++ tetrateci/test_1.8.sh
@@ -1,36 +1,52 @@
-#!env bash
+#!/usr/bin/env bash
+./tetrateci/version_check.py && exit
 set -e
 
 # need this variable to run the tests outside GOPATH
 export REPO_ROOT=$(pwd)
+echo "Set REPO_ROOT=$REPO_ROOT"
+source ./tetrateci/setup_go.sh
 
-git apply tetrateci/patches/common/disable-dashboard.1.8.patch
-git apply tetrateci/patches/common/disable-multicluster.1.8.patch
-git apply tetrateci/patches/common/disable-ratelimiting.1.8.patch
-git apply tetrateci/patches/common/disable-vmospost.1.8.patch
-git apply tetrateci/patches/common/disable-stackdriver.1.8.patch
-
-if $(grep -q "1.17" <<< ${VERSION} ); then
-  git apply tetrateci/common/disable-endpointslice.1.8.patch
-fi
+echo "Applying patches...."
+git apply tetrateci/patches/common/increase-dashboard-timeout.1.8.patch
+git apply tetrateci/patches/common/wait-for-envoy.1.8.patch
+git apply tetrateci/patches/common/increase-vm-timeout.1.8.patch
 
 if [[ ${CLUSTER} == "gke" ]]; then
+  echo "Generating operator config for GKE"
   # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
   pip install pyyaml --user && ./tetrateci/gen_iop.py
   CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
-  git apply tetrateci/patches/gke/chiron-gke.patch
+  
+  if $(grep -q "1.17" <<< ${K8S_VERSION} || grep -q "1.16" <<< ${K8S_VERSION}); then
+    echo "Applying GKE specific patches...."
+    git apply tetrateci/patches/gke/chiron-gke.patch
+  fi
 fi
 
 if [[ ${CLUSTER} == "eks" ]]; then
+  echo "Applying Ingress patch for EKS...."
   git apply tetrateci/patches/eks/eks-ingress.1.8.patch
 fi
 
-if [[ ${CLUSTER} == "aks" ]]; then
-  git apply tetrateci/patches/aks/aks-pilot.1.8.patch
+if $(grep -q "1.17" <<< ${K8S_VERSION} ); then
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /endpointslice | grep -v /stackdriver)
+else
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /stackdriver)
 fi
 
-if $(go version | grep "1.15"); then
-  export GODEBUG=x509ignoreCN=0
-fi
+echo "Starting Testing"
 
-go test -count=1 ./tests/integration/... ${CLUSTERFLAGS} -p 1 -test.v -tags="integ" -timeout 30m
+for package in $PACKAGES; do
+  n=0
+  until [ "$n" -ge 3 ]
+  do
+    echo "========================================================TESTING $package | TRY $n========================================================"
+    go test -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
+    for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
+    n=$((n+1))
+  done
+  [ "$n" -ge 3 ] && exit 1
+done
+
+echo "Testing Done"
--- tetrateci/test_1.9.sh
+++ tetrateci/test_1.9.sh
@@ -1,21 +1,56 @@
-#!env bash
+#!/usr/bin/env bash
+./tetrateci/version_check.py && exit
 set -e
 
-git apply tetrateci/patches/common/disable-dashboard.1.9.patch
-git apply tetrateci/patches/common/disable-multicluster.1.9.patch
-git apply tetrateci/patches/common/disable-ratelimiting.1.9.patch
-git apply tetrateci/patches/common/disable-vmospost.1.9.patch
-git apply tetrateci/patches/common/disable-stackdriver.1.9.patch
+source ./tetrateci/setup_go.sh
+
+echo "Applying patches...."
+
+git apply tetrateci/patches/common/increase-vm-timeout.1.9.patch
+git apply tetrateci/patches/common/increase-sniffing-timeout.1.9.patch
+git apply tetrateci/patches/common/retry-calls-revision-upgrade.1.9.patch
+git apply tetrateci/patches/common/increase-dashboard-timeout.1.9.patch
+
+# the code fails whenever there is something other than digits in the k8s minor version
+# in our case which is a "+" symbol due to extra patching by corresponding vendor
+# so we get 1.17+ instead of 1.17
+git apply tetrateci/patches/common/fix-version-check.1.9.patch
 
 if [[ ${CLUSTER} == "gke" ]]; then
+  echo "Generating operator config for GKE"
   # Overlay CNI Parameters for GCP : https://github.com/tetratelabs/getistio/issues/76
   pip install pyyaml --user && ./tetrateci/gen_iop.py
   CLUSTERFLAGS="-istio.test.kube.helm.iopFile $(pwd)/tetrateci/iop-gke-integration.yml"
-  git apply tetrateci/patches/gke/chiron-gke.patch
+
+  if $(grep -q "1.17" <<< ${K8S_VERSION} || grep -q "1.16" <<< ${K8S_VERSION}); then
+    echo "Applying GKE specific patches...."
+    git apply tetrateci/patches/gke/chiron-gke.patch
+  fi
 fi
 
 if [[ ${CLUSTER} == "eks" ]]; then
+  echo "Applying Ingress patch for EKS...."
   git apply tetrateci/patches/eks/eks-ingress.1.9.patch
 fi
 
-go test -count=1 ./tests/integration/... ${CLUSTERFLAGS} -p 1 -test.v -tags="integ" -timeout 30m
+if $(grep -q "1.17" <<< ${K8S_VERSION}); then
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /endpointslice | grep -v /stackdriver)
+else
+  PACKAGES=$(go list -tags=integ ./tests/integration/... | grep -v /qualification | grep -v /examples | grep -v /multicluster | grep -v /stackdriver)
+fi
+
+echo "Starting Testing"
+
+for package in $PACKAGES; do
+  n=0
+  until [ "$n" -ge 3 ]
+  do
+    echo "========================================================TESTING $package | TRY $n========================================================"
+    go test -test.v -tags=integ $package -timeout 30m --istio.test.select=-postsubmit,-flaky --istio.test.ci --istio.test.pullpolicy IfNotPresent ${CLUSTERFLAGS} && break || echo "Test Failed: $package"
+    for folder in $(ls -d /tmp/* | grep istio); do sudo rm -rf -- $folder; done
+    n=$((n+1))
+  done
+  [ "$n" -ge 3 ] && exit 1
+done
+
+echo "Testing Done"
